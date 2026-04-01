9eb6ce53a17a6582e31677262faa79f6804cd503
Rodrigo de Castro Cardoso <74211714+Rodrigocastrocard@users.noreply.github.com>
2026-04-01 17:22:47 -0300

    integração Gallerist

---
+++ Integração_AP-IP/API APIP.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\Users\Servidor\AppData\Local\Python\pythoncore-3.14-64\python.exe" "C:\Users\Servidor\PycharmProjects\PythonProject\Integração AP-IP\integracao2.py"
+echo.
+echo -------- API FINALIZOU (veja o erro acima) --------
+pause
