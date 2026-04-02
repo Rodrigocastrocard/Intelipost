4f32390a1444eb64fccc9542580186ea295a4bee
Rodrigo de Castro Cardoso <74211714+Rodrigocastrocard@users.noreply.github.com>
2026-04-02 15:12:17 -0300

    integração Gallerist

--- galleristapip/integracao2.py
+++ galleristapip/integracao2.py
@@ -4,6 +4,7 @@ from logging.handlers import RotatingFileHandler
 from flask import Flask, request, jsonify
 from datetime import datetime
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -129,4 +130,5 @@ def health():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
